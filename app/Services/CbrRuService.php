d6d27911d5e2f3969863fbe1c064275553d64d9c
Dmitry Latikov <latikov@gmail.com>
2021-04-16 21:55:13 +0300

    small fixes

--- app/Services/CbrRuService.php
+++ app/Services/CbrRuService.php
@@ -13,6 +13,12 @@ class CbrRuService
 {
     protected const CBR_RU_ENDPOINT = 'https://www.cbr.ru/scripts/XML_daily.asp';
 
+    /**
+     * Request rates array from cbr.ru
+     * @param Carbon $carbonDate
+     * @return array
+     * @throws \Exception
+     */
     protected function requestCbrRu(Carbon $carbonDate) : array
     {
         $response = Http::get(self::CBR_RU_ENDPOINT, [
@@ -31,11 +37,14 @@ class CbrRuService
             $dayData[(string)$valuteXml->CharCode] = (string)$valuteXml->Value;
         }
 
-        //dd($simpleXml);
-
         return $dayData;
     }
 
+    /**
+     * Return rates array for $day using db caching
+     * @param string $day
+     * @return array
+     */
     public function fetchByDay(string $day) : array
     {
         $carbonDate = Carbon::createFromFormat("Y-m-d", $day);
